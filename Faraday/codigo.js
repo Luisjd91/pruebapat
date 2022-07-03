1192a38f0e6c75f54ce63db8a4a7e1f4a69d4ec3
luisjdominguez02@gmail.com <luisjdominguez02@gmail.com>
2022-07-03 01:43:51 -0300

    probandopat

---
+++ Faraday/codigo.js
@@ -0,0 +1,32 @@
+dineroCofla= prompt ("cuando tinero tienes cofla?");
+
+if (dineroCofla >= 0.6 && dineroCofla <1) {
+	alert ("Comprate el helado de agua");
+}
+
+else if (dineroCofla >= 1 && dineroCofla <1.6) {
+
+alert ("Comprate el helado de crema")
+}
+
+else if (dineroCofla >= 1.6 && dineroCofla <1.7) {
+
+alert ("Comprate el helado marca heladix")
+}
+
+else if (dineroCofla >= 1.7 && dineroCofla <1.8) {
+
+alert ("Comprate el helado marca heladovich")
+}
+
+else if (dineroCofla >= 1.8 && dineroCofla <2.9) {
+
+alert ("Comprate el helado marca helardo")
+}
+
+else if (dineroCofla >= 2.9) {
+	alert ("helado con confites o el pote de 1/4 kg");
+}
+else {
+	alert ("lo siento papu no te alcanza para nada")
+	}
